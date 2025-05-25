0c01f0aaa6d8fbbd807ac47d0599faf126ea7467
abc <abc@naver.com>
2025-05-25 12:45:58 +0900

    fix

---
+++ basic_syntax/2.env_setting.sh
@@ -0,0 +1,12 @@
+git init
+
+#원격지 주소를 생성 및 추가
+git remote add origin 레포 주소
+
+#원격지 주소 삭제
+git rmote remove origin 레포 주소
+
+#사용자 지정 방법
+#전역적 사용자 (이름, eamil)지정
+git config --global user.name "유저 네임"
+git config --global user.email "유저 이메일"
